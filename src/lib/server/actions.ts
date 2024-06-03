124eb45a0a731e260fa0edd26019bcbd9fb489cc
Bishwas Bhandari <bishwasbh@gmail.com>
2024-06-03 09:51:22 +0545

    path suffix added

--- src/lib/server/actions.ts
+++ src/lib/server/actions.ts
@@ -121,9 +121,10 @@ export const via_route =
             proxy_action.path = opt_.prefix + proxy_action.path;
             const action = {
                 [proxy_action.path]: async (event: RequestEvent) => {
+                    const p_sfx = event.url.searchParams.get("p_sfx") || ""; // path suffix
                     try {
                         const form_data = await event.request.formData();
-                        let url = `${opt_.django_base_api}/${proxy_action.path}`;
+                        let url = `${opt_.django_base_api}/${proxy_action.path}${p_sfx}`;
                         let options: RequestInit = {method: proxy_action.method}
                         if (proxy_action.method === 'GET') {
                             url = `${url}?${new URLSearchParams(form_data as any).toString()}`;
